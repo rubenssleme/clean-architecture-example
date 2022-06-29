f7ae7cbc2b77d7debb57f822d97d5213e2790c27
Rubens Leme <rubenss.leme@gmail.com>
2022-06-29 09:31:20 -0300

    ajuste

---
+++ src/test/java/com/example/cleanarch/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cleanarch.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
